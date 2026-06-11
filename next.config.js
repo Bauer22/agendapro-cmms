77f6f2e80d1e55f40dfb1242e0955b760f98da2e
Bauer22 <128606776+Bauer22@users.noreply.github.com>
2026-06-11 09:15:20 -0300

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,1 @@
-// Industrial8 v1.0 build 1781140022
- v6.0.0 build 1781032685.5477657
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  images: { unoptimized: true },
-}
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */`nconst nextConfig = { typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true }, images: { unoptimized: true } };`nmodule.exports = nextConfig
